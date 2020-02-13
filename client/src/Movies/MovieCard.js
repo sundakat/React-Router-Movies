b4647cbefdd28938fc864c0de3c1097d7fbeccc1
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-02-13 15:13:22 -0600

    Set up MovieCard

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from 'react'
 
-const MovieCard = props => {
-  return;
-};
+const MovieCard = (props) => {
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{props.title}</h2>
+        <div className="movie-director">
+          Director: <em>{props.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{props.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+        {props.stars.map((star) => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      {props.addToSavedList && (
+        <div className="save-button" onClick={props.addToSavedList}>
+          Save
+                </div>
+      )}
+    </div>
+  )
+}
 
 export default MovieCard;
